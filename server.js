41cd7d481961d17456256b9531fed9a007bde620
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-21 20:23:22 -0700

    finishing dev env setup for server to run, no routes in place but comment models in place to get the idea. we also need models to do any CRUD functionality, but that comes after the ERD verification

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/paws_controller.js");
 
-app.use(routes);
+// app.use(routes);
 
 
 var PORT = process.env.PORT || 3000;
