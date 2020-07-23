65c12403e020296d53808fb1825188bcc3ab5e36
Nicole Remy <nicoleremy95@gmail.com>
2020-07-23 14:32:42 -0700

    finished event listeners, dummy ajax requests, and redirects after each button or submit

--- public/assets/js/user_hb.js
+++ public/assets/js/user_hb.js
@@ -1,5 +1,6 @@
 //EVENT LISTENERS
 
+//owner.handlebars
 //when the user clicks on add dog, they will be redirected to an add dog page with a form
 $(".add-dog").click(function(event){
   event.preventDefault();
@@ -8,7 +9,6 @@ $(".add-dog").click(function(event){
 
   console.log(" clicked add dog")
 })
-
 //when the user clicks on add cat, they will be redirected to an add cat page with a form
 $(".add-cat").click(function(event){
   event.preventDefault();
@@ -17,15 +17,15 @@ $(".add-cat").click(function(event){
   console.log(" clicked add cat")
 })
 
+
+//professional.handlebars
 //when the user clicks on service dog, they will be redirected to a service dog page with a form
 $(".service-dog").click(function(event){
   event.preventDefault();
   $(".service-dogs-page").toggle();
   $(".service-cats-page").hide();
-
   console.log(" clicked service dog")
 })
-
 //when the user clicks on service cat, they will be redirected to a service cat page with a form
 $(".service-cat").click(function(event){
   event.preventDefault();
@@ -33,29 +33,187 @@ $(".service-cat").click(function(event){
   $(".service-cats-page").toggle();
   console.log(" clicked service cat")
 })
-//when the user clicks on save account, they will be redirected to a service cat page with a form
-$("#save-account").click(function(event){
-  event.preventDefault();
 
-  console.log(" clicked save account")
 
-})
-//when the user clicks on service cat, they will be redirected to a service cat page with a form
-$("#delete-account").click(function(event){
-  event.preventDefault();
 
-  console.log(" clicked delete account")
-})
-//when the user clicks on service cat, they will be redirected to a service cat page with a form
-$("#create-account").click(function(event){
-  event.preventDefault();
-
-  console.log(" clicked create account")
-})
-//when the user clicks on service cat, they will be redirected to a service cat page with a form
+//USER ACOUNTS
+//signin.handlebars
+//when the user clicks on service cat, they will be redirected to the home page
 $("#sign-in").click(function(event){
   event.preventDefault();
-
+  location.href = '/'
   console.log(" clicked sign in ")
+})
+
+
+//account-profile.handlebars
+//when the user clicks on save account, they will be redirected to their owner page
+$("#save-account").click(function(event){
+  event.preventDefault();
+  const accountId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/account/${accountId}`,
+    method: "PUT"
+  }).then(data=>{
+    alert("deleted!");
+    ocation.href = `/user/owner/${id}`
+  })
+  console.log(" clicked save account")
+})
+//when the user clicks on service cat, they will be redirected to their owner page
+$("#delete-account").click(function(event){
+  event.preventDefault();
+  const accountId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/account/${accountId}`,
+    method: "DELETE"
+  }).then(data=>{
+    alert("deleted!");
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked delete account")
+})
+
+
+//createaccount.handlebars
+//when the user clicks on create account, they will be redirected their owner page
+$("#create-account").click(function(event){
+  event.preventDefault();
+  const accountId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/account/${accountId}`,
+    method: "POST"
+  }).then(data=>{
+    alert("POSTED!");
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked create account")
+})
+
+
+
+
+//PROFESSIONALS AND THEIR SERVICES
+//servicecat.handlebars
+//when the user clicks on submit, they will be redirected to their professional page 
+$("#service-cat-form").click(function(event){
+  event.preventDefault();
+  const serviceCatId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/service/${serviceCatId}`,
+    method: "POST"
+  }).then(data=>{
+    alert("POSTED cat service!");
+    location.href = `/user/professional/${id}`
+  })
+  console.log(" clicked service cat form")
+})
+
+
+//servicedog.handlebars
+//when the user clicks on submit, they will be redirected to their professional page
+$("#service-dog-form").click(function(event){
+  event.preventDefault();
+  const serviceDogId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/service/${serviceDogId}`,
+    method: "POST"
+  }).then(data=>{
+    alert("POSTED dog service!");
+    location.href = `/user/professional/${id}`
+  })
+  console.log(" clicked service dog form")
+})
+
+
+
+//active-postings.handlebars
+//when the user clicks on submit, they will be redirected to their professional page 
+$("#save-posting").click(function(event){
+  event.preventDefault();
+  const postingId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/service/${postingId}`,
+    method: "PUT"
+  }).then(data=>{
+    alert(`saved ${postingId} posting!`);
+    location.href = `/user/professional/${id}`
+  })
+  console.log(" clicked save posting")
+})
+//when the user clicks on submit, they will be redirected to their professional page 
+$("#delete-posting").click(function(event){
+  event.preventDefault();
+  const postingId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/service/${postingId}`,
+    method: "DELETE"
+  }).then(data=>{
+    alert(`saved ${postingId} posting!`);
+    location.href = `/user/professional/${id}`
+  })
+  console.log(" clicked delete posting")
+})
+
+
+//OWNERS AND THEIR PETS
+//addcat.handlebars
+//when the user clicks on submit, they will be redirected to their owner page 
+$("#add-cat-form").click(function(event){
+  event.preventDefault();
+  const addCatId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/user/pets/${addCatId}`,
+    method: "POST"
+  }).then(data=>{
+    alert("POSTED new cat!");
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked add cat form")
+})
+
+
+//adddog.handlebars
+//when the user clicks on submit, they will be redirected to their owner page  
+$("#add-dog-form").click(function(event){
+  event.preventDefault();
+  const addDogId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api//user/pets/${addDogId}`,
+    method: "POST"
+  }).then(data=>{
+    alert("POSTED new dog !");
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked add dog form")
+})
+
+
+//pet-family.handlebars
+//when the user clicks on submit, they will be redirected to their owner page 
+$("#save-pet").click(function(event){
+  event.preventDefault();
+  const petId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/user/pets/${petId}`,
+    method: "PUT"
+  }).then(data=>{
+    alert(`saved ${petId} posting!`);
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked save pet")
+})
+//when the user clicks on submit, they will be redirected to their owner page
+$("#delete-pet").click(function(event){
+  event.preventDefault();
+  const petId = $(this).attr("data-id")
+  $.ajax({
+    url:`/api/user/pets/${petId}`,
+    method: "DELETE"
+  }).then(data=>{
+    alert(`saved ${petId} posting!`);
+    location.href = `/user/owner/${id}`
+  })
+  console.log(" clicked delete pet")
 })
 
