c48ff93ef83ac1672141cbe487526df47b47a1d3
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-22 17:32:28 -0700

    working on routes, adding notes for standup regarding certain vars and model attributes

--- controllers/paws_controller.js
+++ controllers/paws_controller.js
@@ -17,7 +17,64 @@ router.get("/user", function(req, res) {
   return res.render("user");
 });
 
+// TODO:EVWORKSPACE--------------------------------------------------------
 
+//offer_posts CREATE NEW POST
+router.post("/offer_posts/create", function(req,res) {
+  
+  db.offerPost.create(
+    req.body
+    //might have to expand req.body later into component parts depending on what we are grabbing from each input. 
+  )
+  .then(function(dbPost) {
+      console.log(dbPost);
+      //TODO: decide where this redirect will go, ask Nicole. for now will refresh the total posts page.
+      res.redirect("/offer_posts");
+    });
+})
+
+//offer_posts READ ALL
+router.get("/offer_posts", function(req,res) {
+  
+  db.offerPost.findAll()
+  
+  .then(function(dbPost) {
+    console.log(dbPosts);
+    const dbPostsJson = dbPosts.map(post=>post.toJSON());
+    let hbrsObj = { offer_post : dbPostsJson };
+    // return res.render("offer_post", hbrsObj);
+    //TODO: convert the below to above line when post.handlebars exists
+    return res.json(hbrsObj);
+  })
+})
+
+//TODO: add in several more findAll() WHERE: range < X will be trickier than normal - we will have to do a find all, then filter and calculate in a for loop. might be able to do that? or maybe just inputing a value range limit 
+
+// offer_posts UPDATE method, by post id.
+router.put("offer_posts/update:id", function (req,res) {
+
+  db.offerPost.update({
+    title: req.body.title,
+    text: req.body.text,
+    size_restrictions: req.body.size_restrictions,
+    animal_type: req.body.animal_type,
+    duration: req.body.duration,
+    range: req.body.range,
+    //TODO: verify need for range. may have to mathematically convert "range" into an over/under in the lat and long comparisons.
+    cost: req.body.cost,
+    service_type: req.body.service_type,
+    //TODO: verify how pictures will be used
+    pictures: req.body.pictures,
+  },
+  {where: {
+    id: req.params.id
+  }})
+}).then(function(dbPosts){
+  res.json(`changed the post with id of ${req.params.id}`)
+})
+
+
+// TODO:EVWORKSPACE----------------------------------------------------------------
 
 
 // router.get("/burgers", function(req, res) {
@@ -40,7 +97,7 @@ router.get("/user", function(req, res) {
 //       res.redirect("/");
 //     });
 // });
-
+//offer posting update 
 
 // router.put("/burgers/update/:id", function(req, res) {
 //   db.Burger.update({
