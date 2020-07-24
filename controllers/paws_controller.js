beea28e5c7bfde9282366fe4e4e708c1d2f95986
Nicole Remy <nicoleremy95@gmail.com>
2020-07-24 09:45:21 -0700

    fixed image file path

--- controllers/paws_controller.js
+++ controllers/paws_controller.js
@@ -6,46 +6,8 @@ var router = express.Router();
 var db = require("../models");
 
 
-//main route welcome
-router.get("/", function(req, res) {
-  return res.render("index");
-});
-
-
-//redirects you to the user owner page as a default and will need the user :id param
-router.get("/user", function(req, res) {
-    return res.redirect("/user/owner");
-  });
-  
-
-//takes you to the sign in page
-router.get("/signin", function(req, res) {
-  return res.render("signin");
-});
-
-//takes you to the createaccount page 
-router.get("/createaccount", function(req, res) {
-  return res.render("createaccount");
-});
-
-//this route will need to include a :id at the end so it goes to the specific user page
-router.get("/user/professional", function(req, res) {
-  return res.render("professional", {name:"nicole"});
-});
-
-//this route will need to include a :id at the end so it goes to the specific user page
-router.get("/user/owner", function(req, res) {
-  return res.render("owner", {name:"nicole"});
-});
-
-//gets user account profile. this route will need to include a :id at the end so it goes to the specific user page
-router.get("/user/account-profile", function(req, res) {
-  return res.render("account-profile", {name:"nicole"});
-});
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~HERE BEGIN THE ROUTES FOR USERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//users
+// create users 
 router.post("/users/create", function(req, res) {
   db.User.create({
    
@@ -58,7 +20,6 @@ router.post("/users/create", function(req, res) {
     // long: req.body.long
   }).then(function(dbUser) {
       console.log(dbUser);
-      // res.redirect("/Users");
       res.json(dbUser)
     }).catch(function(err){
      
@@ -67,21 +28,19 @@ router.post("/users/create", function(req, res) {
     
 });
 
-//this route pulls a specific user based on their ID, will be useful in redirecting user to their page after account creation as well as user selecting themselves/logging in
+// //get user by id 
 router.get("/users/:id", function(req, res) {
   db.User.findOne({
     where: {id: req.params.id}
   }).then(function(dbUser) {
     console.log(dbUser);
-    // res.json(dbUser, `retrieved user with id: ${req.params.id}`);
     res.json( dbUser)
-    //TODO: check in with nicole where this will route to, pulling functional record correctly based on user id.
   }).catch(function(err){
     res.status(500).json(err);
   });
 });
 
-//this update route for users works, just needs to be res redirected properly via Nicole's plan
+// update user by id 
 router.put("/users/update/:id", function(req, res) {
   db.User.update(req.body,
   {
@@ -90,21 +49,19 @@ router.put("/users/update/:id", function(req, res) {
     }
   }
   ).then(function(dbUsers) {
-    //this should redirect us to homepage, may have to edit.
-    // res.json("/");
     res.json(dbUsers);
   }).catch(function(err){
     res.status(500).json(err);
   });
 });
 
+//delete user by id 
 router.delete("/users/delete/:id", function (req, res) {
   db.User.destroy({
     where: {
       id: req.params.id
     }
   }).then(function (dbUsers) {
-    // res.json(dbUsers);
     res.json(`destroyed the user account with id of ${req.params.id}`);
   }).catch(function(err){
     res.status(500).json(err);
@@ -130,8 +87,6 @@ router.post("/offer_posts/create", function(req,res) {
   })
   .then(function(dbPost) {
       console.log(dbPost);
-      //TODO: decide where this redirect will go, ask Nicole. for now will refresh the total posts page.
-      // res.redirect("/offer_posts");
       res.json(dbPost)
     }).catch(function(err){
       res.status(500).json(err);
@@ -146,8 +101,6 @@ router.get("/offer_posts", function(req,res) {
   .then(function(dbPost) {
     console.log(dbPost);
     let hbrsObj = { offer_posts : dbPost };
-    // return res.render("offer_post", hbrsObj);
-    //TODO: convert the below to above line when post.handlebars exists or we know where it's rendering.
     return res.json(hbrsObj);
   }).catch(function(err){
     res.status(500).json(err);
@@ -167,7 +120,6 @@ router.put("/offer_posts/update/:id", function (req,res) {
     range: req.body.range,
     cost: req.body.cost,
     service_type: req.body.service_type,
-    //TODO: verify how pictures will be used
     pictures: req.body.pictures,
   },
   {where: {
@@ -179,7 +131,7 @@ router.put("/offer_posts/update/:id", function (req,res) {
 }).catch(function(err){
   console.log(err);
   res.status(500).json(err);
-});;
+});
 });
 
 // offer_posts DELETE, by post id.
@@ -206,7 +158,7 @@ router.delete("/offer_posts/:id", function (req, res) {
 
 
 // //=====================HERE BEGIN THE ROUTES FOR THE PETS=========================
-// pets CREATE TODO: untested!
+// //create pet 
 router.post("/pets/create", function(req,res) {
   
   db.Pet.create({
@@ -221,8 +173,6 @@ router.post("/pets/create", function(req,res) {
   })
   .then(function(dbPet) {
       console.log(dbPet);
-      //TODO: decide where this redirect will go, ask Nicole. for now will take us to /pets
-      // res.redirect("/pets");
       res.json(dbPet);
     }).catch(function(err){
       console.log(err);
@@ -230,7 +180,7 @@ router.post("/pets/create", function(req,res) {
     });
 })
 
-
+// //get all pets
 router.get("/pets", function(req,res) {
   db.Pet.findAll()
   .then(function(dbPet) {
@@ -242,14 +192,13 @@ router.get("/pets", function(req,res) {
   });
 });
 
-
+// //get pet by id 
 router.get("/pets/:id", function (req,res) {
  db.Pet.findOne({
     where: {id:req.params.id}
   }).then(function(dbPet){
     // const dbPetsJson = dbPet.map(pet=>pet.toJSON());
     // var hbrsObject = { pet: dbPetsJson };
-    // return res.render("nICOLESlAYOUThANDLEbARhERE", hbrsObject) //TODO: turn this into a real subframe render.
     return res.json({pet: dbPet})
   }).catch(function (err) {
     res.status(500).json(err);
@@ -257,9 +206,7 @@ router.get("/pets/:id", function (req,res) {
 
 })
 
-
-
-//pets UPDATE    TODO: untested! please test and revise
+// //update pet by id
 router.put("/pets/update/:id", function (req,res) {
 
   db.Pet.update({
@@ -283,6 +230,7 @@ router.put("/pets/update/:id", function (req,res) {
 });
 });
 
+// //delete pet by id
 router.delete("/pets/:id", function (req, res) {
   db.Pet
     .destroy({
@@ -299,12 +247,13 @@ router.delete("/pets/:id", function (req, res) {
 
 });
 
-
-// //TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
 // //=====================HERE END THE ROUTES FOR THE PETS===================================
 
-//==============================HERE BEGIN THE RATING ROUTES===========================================
 
+
+
+//==============================HERE BEGIN THE RATING ROUTES===========================================
+// create rating 
 router.post("/ratings/create", function (req, res) {
   db.Rating.create({
     service_name : req.body.service_name,
@@ -312,28 +261,25 @@ router.post("/ratings/create", function (req, res) {
     rating : req.body.rating
   }).then(function (dbRating) {
         console.log(dbRating);
-        // res.redirect("/rating");
-        //TODO: link this properly with the frontend, both on req.body having an id key (or whatever it's called)
         res.json(dbRating);
       }).catch(function(err){
         res.status(500).json(err);
       });
     });
 
+// get all ratings 
 router.get("/ratings", function (req,res) {
   db.Rating.findAll({
     include: [db.User, db.Pet]
   }).then(function (dbRating){
     console.log(dbRating)
-    //   //TODO: make sure this render is correct
-    //   return res.render("index", allCustomer);
     return res.json(dbRating)
   }).catch(function(err){
     res.status(500).json(err);
   });
 })
 
-
+// get ratings by id
 router.get("/ratings/:id", function (req,res) {
   db.Rating.findOne({
     where: {id: req.params.id}
@@ -341,7 +287,6 @@ router.get("/ratings/:id", function (req,res) {
   .then(function(dbRating) {
     // const dbRatingJson = (dbRating=>Rating.toJSON());
     // var oneRating = { Rating: dbRatingJson };
-    //TODO: make sure this render is correct
     return res.json({rating: dbRating});
   })
   .catch(function(err){
@@ -349,6 +294,7 @@ router.get("/ratings/:id", function (req,res) {
   });
 })
 
+// update rating by id 
 router.put("/ratings/update/:id", function (req,res) {
   db.Rating.update({
     service_name: req.body.service_name,
@@ -367,13 +313,13 @@ router.put("/ratings/update/:id", function (req,res) {
 });;
 });
 
+// delete rating by id
 router.delete("/ratings/delete/:id", function (req, res) {
   db.Rating.destroy({
     where: {
       id: req.params.id
     }
   }).then(function (dbRating) {
-    // res.json(dbUsers);
     res.json(`destroyed the rating with id of ${req.params.id}`);
   }).catch(function(err){
     res.status(500).json(err);
