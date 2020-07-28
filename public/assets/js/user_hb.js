518d51b8624d2f512807114a6b7636d1c7bb788e
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-27 23:34:33 -0700

    all conflicts finally resolved, working on last basic styling

--- public/assets/js/user_hb.js
+++ public/assets/js/user_hb.js
@@ -10,69 +10,73 @@ $("#sign-in").click(function (event) {
   const userObj = {
     user_name: $("#signinUsername").val(),
     password: $("#signinPassword").val(),
-  }
+  };
   console.log("User Obj: " + userObj);
 
   $.ajax("/signin", {
     type: "POST",
-    data: userObj
-  }).done(data => {
-    alert("welcome back!");
-    location.href = '/'
-    console.log(data)
-  }).fail(function (err) {
-    console.log(err);
-    alert("check your username or password!")
-    location.reload();
+    data: userObj,
   })
-  location.href = '/'
-  console.log(" clicked sign in ")
-})
+    .done((data) => {
+      alert("welcome back!");
+      location.href = "/";
+      console.log(data);
+    })
+    .fail(function (err) {
+      console.log(err);
+      alert("check your username or password!");
+      location.reload();
+    });
+  location.href = "/";
+  console.log(" clicked sign in ");
+});
 
 //auth_controller.js
 //logout
-$("#logout").click(function(){
-  alert("logged out of account")
-  console.log("clicked log out")
-})
+$("#logout").click(function () {
+  alert("logged out of account");
+  console.log("clicked log out");
+});
 
 //account-profile.handlebars
 //when the user clicks on save account, they will be redirected to their owner page
 $("#save-account").click(function (event) {
   event.preventDefault();
-  const accountId = $(this).attr("data-id")
+  const accountId = $(this).attr("data-id");
   const userObj = {
-    first_name:$("#edit-first-name").val(),
-    last_name:$("#edit-last-name").val(),
-    user_name:$("#edit-username").val(),
-    email:$("#edit-email").val(),
-  }
+    first_name: $("#edit-first-name").val(),
+    last_name: $("#edit-last-name").val(),
+    user_name: $("#edit-username").val(),
+    email: $("#edit-email").val(),
+  };
   $.ajax({
-    url:`/users/update/${accountId}`,
+    url: `/users/update/${accountId}`,
     method: "PUT",
-    data: userObj
-  }).then(data=>{
+    data: userObj,
+  }).then((data) => {
     alert("saved account!");
-    location.href = `/`
-  })
-  console.log(" clicked save account")
-})
+    location.href = `/`;
+  });
+  console.log(" clicked save account");
+});
 //when the user clicks on service cat, they will be redirected to their owner page
 $("#delete-account").click(function (event) {
   event.preventDefault();
-  const accountId = $(this).attr("data-id")
+  const accountId = $(this).attr("data-id");
   $.ajax({
     url: `/users/delete/${accountId}`,
-    method: "DELETE"
-  }).done(data => {
-    alert("account deleted!");
-    location.href = "/"
-  }).fail(err => {
-    alert("something went wrong");
-    window.location.reload();
+    method: "DELETE",
   })
-  console.log(" clicked delete account")
-})
+    .done((data) => {
+      alert("account deleted!");
+      location.href = "/";
+    })
+    .fail((err) => {
+      alert("something went wrong");
+      window.location.reload();
+    });
+  console.log(" clicked delete account");
+});
 
 //createaccount.handlebars
 //when the user clicks on create account, they will be redirected their owner page
@@ -84,11 +88,10 @@ $("#create-account").on("click", function (event) {
 
   $.ajax({
     url: "https://ipapi.co/json/",
-    method: "GET"
-
+    method: "GET",
   }).then(function (response) {
-    latitude =parseFloat( response.latitude);
-    longitude =parseFloat(  response.longitude);
+    latitude = parseFloat(response.latitude);
+    longitude = parseFloat(response.longitude);
     console.log("response from location ajax", response);
     console.log("latitude saved from location ajax", latitude);
     console.log("longitude saved from location ajax", longitude);
@@ -99,69 +102,69 @@ $("#create-account").on("click", function (event) {
       password: $("#new-password").val(),
       email: $("#email").val(),
       lat: latitude,
-      long: longitude
-    }
+      long: longitude,
+    };
 
-    console.log( userObj);
+    console.log(userObj);
     $.ajax("/createaccount", {
       type: "POST",
-      data: userObj
-    }).done(data => {
-      alert("ACCOUNT CREATED!");
-      location.href = '/signin'
-      console.log(data)
-    }).fail(function (err) {
-      console.log(err);
-      alert("something went wrong")
-      location.reload();
+      data: userObj,
     })
+      .done((data) => {
+        alert("ACCOUNT CREATED!");
+        location.href = "/signin";
+        console.log(data);
+      })
+      .fail(function (err) {
+        console.log(err);
+        alert("something went wrong");
+        location.reload();
+      });
 
-    console.log(" clicked create account")
+    console.log(" clicked create account");
   });
+});
+  //rickybranch inclusion, left commented
+  // $(".claimPost").click(function (event) {
+  //   event.preventDefault();
+  //   const postId = $(this).attr("data-id")
+  //   console.log(postId)
+  //   $.ajax({
+  //     url: `/offer_posts/${postId}/claimpost`,
+  //     method: "PUT",
 
+  //   }).then(data => {
+  //     console.log(data)
+  //     location.href = `/`;
+  //   })
+  // })
 
-//rickybranch inclusion, left commented
-// $(".claimPost").click(function (event) {
-//   event.preventDefault();
-//   const postId = $(this).attr("data-id")
-//   console.log(postId)
-//   $.ajax({
-//     url: `/offer_posts/${postId}/claimpost`,
-//     method: "PUT",
-
-//   }).then(data => {
-//     console.log(data)
-//     location.href = `/`;
-//   })
-// })
-  
-//dev branch,
+  //dev branch,
+  //kept this instead. we can clean this code later,
 //kept this instead. we can clean this code later, 
-//but I wanted to leave both versions handy after this afternoon.
-//MAIN PAGE SEARCH
+  //kept this instead. we can clean this code later,
+  //but I wanted to leave both versions handy after this afternoon.
+  //MAIN PAGE SEARCH
+  //book a service
 //book a service 
+  //book a service
 
-$("#book-now").click(function(event){
-
+$("#book-now").click(function (event) {
   event.preventDefault();
-  const postId = $(this).attr("data-id")
-  
-  console.log(postId)
+  const postId = $(this).attr("data-id");
+
+  console.log(postId);
   $.ajax({
-
-    url:`/offer_posts/${postId}/bookpost`,
-    method:"PUT",
-    
-  }).then(data=>{
-
-    console.log(data)
-    alert("booked!")
+    url: `/offer_posts/${postId}/bookpost`,
+    method: "PUT",
+  }).then((data) => {
+    console.log(data);
+    alert("booked!");
     location.href = `/`;
-  })
-})*/
-
-
-  $("dog-search").on("click", function (event) {
+  });
+});
+//TODO:unify this search field, which means unifying button terminology, removing radio buttons to just describe the search.
+$("dog-search").on("click", function (event) {
   event.preventDefault();
 
   let latitude;
@@ -173,13 +176,13 @@ $("#book-now").click(function(event){
   }).then(function (response) {
     latitude = response.latitude;
     longitude = response.longitude;
-    const petId = $(this).attr("data-id")
+    const petId = $(this).attr("data-id");
     console.log(petId);
     $.ajax({
       url: `/offer_posts/dog/${latitude}/${longitude}`,
       method: "GET",
-    }).then(data => {
-      console.log(data)
+    }).then((data) => {
+      console.log(data);
 
       var infowindow = new google.maps.InfoWindow();
 
@@ -188,26 +191,33 @@ $("#book-now").click(function(event){
       for (i = 0; i < latLong.length; i++) {
         marker = new google.maps.Marker({
           position: new google.maps.LatLng(latitude, longitude),
-          map: map
+          map: map,
         });
 
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        google.maps.event.addListener(
+          marker,
+          "click",
+          (function (marker, i) {
+            return function () {
+              infowindow.setContent(locations[i][0]);
+              infowindow.open(map, marker);
+            };
+          })(marker, i)
+        );
+      }
+      google.maps.event.addListener(
+        marker,
+        "click",
+        (function (marker, i) {
           return function () {
             infowindow.setContent(locations[i][0]);
             infowindow.open(map, marker);
-          }
-        })(marker, i));
-      }
-      google.maps.event.addListener(marker, "click", (function (marker, i) {
-        return function () {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    })
-  })
-})
-
+          };
+        })(marker, i)
+      );
+    });
+  });
+});
 
 //floating button navigation
 $(document).ready(function(){
@@ -215,9 +225,7 @@ $(document).ready(function(){
 });
 
 
-
-
-})
+//TODO: note, there was a }) here, that looked like it belonged up by the end of the create account f(x).
 
 
 // $(".material-icons").click(function (event) {
